fd894e9ef56838c2fd64a0bdb2d7b9e24ba34792
Henrik Blidh <henrik.blidh@nedomkull.com>
2016-04-26 08:18:18 +0200

    Implemented apply. Wrote first documentation and docstrings. Fixed coveralls errors.

--- README.rst
+++ README.rst
@@ -7,7 +7,8 @@ Calibraxis
     :target: https://coveralls.io/github/hbldh/calibraxis?branch=master
 
 An Python/NumPy implementation of the accelerometer calibration method
-described in  [#FRO2009]_.
+described in  [#FRO2009]_. This is a Python reimplementation of the
+Matlab routine found at [#MLCENTRAL]_.
 
 Installation
 ------------
@@ -20,7 +21,27 @@ Installation
 Usage
 -----
 
-TBW.
+.. code-block:: python
+
+    import numpy as np
+    from calibraxis import Calibraxis
+
+    # Measuring range +/-8g
+    c = Calibraxis(8)
+    points =np.array([[-4772.38754098, 154.04459016, -204.39081967],
+                      [3525.0346179, -68.64924886, -34.54604833],
+                      [-658.17681729, -4137.60248854, -140.49377865],
+                      [-564.18562092, 4200.29150327, -130.51895425],
+                      [-543.18289474, 18.14736842, -4184.43026316],
+                      [-696.62532808, 15.70209974, 3910.20734908],
+                      [406.65271419, 18.46827992, -4064.61085677],
+                      [559.45926413, -3989.69513798, -174.71879106],
+                      [597.22629169, -3655.54153041, -1662.83257031],
+                      [1519.02616089, -603.82472204, 3290.58469588]])
+    c.add_points(points)
+    c.calibrate_accelerometer()
+    c.apply(points[0 :])
+    >>> (-0.9998374717802275, 0.018413117166568103, -0.015581921828828033)
 
 Testing
 -------
@@ -49,3 +70,6 @@ References
     "Autocalibration of MEMS Accelerometers," Instrumentation and Measurement,
     IEEE Transactions on, vol.58, no.6, pp.2034,2041, June 2009
     <http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=4655611&isnumber=4919430>`_
+
+.. [#MLCENTRAL] `Matlab File Central <http://se.mathworks.com/matlabcentral/
+    fileexchange/33252-mems-accelerometer-calibration-using-gauss-newton-method>`_.
--- calibraxis.py
+++ calibraxis.py
@@ -20,13 +20,14 @@ import six
 import numpy as np
 
 # Version information.
-__version__ = '0.1.0'
+__version__ = '0.1.1.dev1'
 version = __version__  # backwards compatibility name
-version_info = (0, 1, 0)
+version_info = (0, 1, 0, 'dev1')
 
 
 class Calibraxis(object):
-    """Calibration object, used for storing the calibration parameters.
+    """Calibration object, used for storing and applying the
+    calibration parameters.
 
     :param int measuring_range: The operational range of the accelerometer,
         e.g. 8 in the case of +/- 8g range.
@@ -37,7 +38,6 @@ class Calibraxis(object):
     """
 
     def __init__(self, measuring_range, resolution=None, verbose=False):
-
         self._points = []
         self._resolution = resolution
         self._range = measuring_range
@@ -50,21 +50,13 @@ class Calibraxis(object):
         self.acc_bias_vector = None
         self.acc_scale_factor_matrix = None
 
-        if self._resolution is not None:
-            self.__mid_v = 2 ** (resolution - 1)
-            self.__max_v = (2 ** resolution) - 1
-            self._acc_zero_g = 0.5 * np.ones((3, ), 'float')
-            self._acc_sensitivity = self._range / 0.5 * np.ones((3, ), 'float')
-        else:
-            self.__mid_v = 0
-            self.__max_v = 1.0
-            self._acc_zero_g = np.zeros((3, ), 'float')
-            self._acc_sensitivity = np.ones((3, ), 'float')
-
-    def acc_to_ratio(self, x):
-        return (x + self.__mid_v) / self.__max_v
-
     def add_points(self, points):
+        """Add point(s) to the calibration procedure.
+
+        :param list, tuple, numpy.ndarray points: The point(s) to add to the
+            calibration point storage.
+
+        """
         if isinstance(points, (list, tuple)):
             if len(points) > 0:
                 if isinstance(points[0], (list, tuple, np.ndarray)):
@@ -85,7 +77,7 @@ class Calibraxis(object):
                 self._points.append(points.copy())
 
     def calibrate_accelerometer(self):
-        """Perform calibration of accelerometer.
+        """Perform calibration of accelerometer using stored points.
 
         Computes the Zero G levels, Sensitivity, Scale factor Matrix and the
         bias vector of a MEMS accelerometer.
@@ -99,9 +91,6 @@ class Calibraxis(object):
 
         The mathematical model used is  A = M(V - B)
         where M and B are scale factor matrix and bias vector respectively.
-        Note that the vector V has elements in [0, 1.0] representing the
-        normed output value of the sensor. This is calculated by
-        `(a_raw + (2 ** 15)) / ((2 ** 16) - 1)`.
 
         M = [ Mxx Mxy Mxz; Myx Myy Myz; Mzx Mzy Mzz ]
         where  Mxy = Myx; Myz = Mzy; Mxz = Mzx;
@@ -116,7 +105,8 @@ class Calibraxis(object):
         Reference:
         Iuri Frosio, Federico Pedersini, N. Alberto Borghese
         "Autocalibration of MEMS Accelerometers"
-        IEEE TRANSACTIONS ON INSTRUMENTATION AND MEASUREMENT, VOL. 58, NO. 6, JUNE 2009
+        IEEE TRANSACTIONS ON INSTRUMENTATION AND MEASUREMENT,
+        VOL. 58, NO. 6, JUNE 2009
 
         This is a Python reimplementation of the Matlab routines found at
         `Matlab File Central <http://se.mathworks.com/matlabcentral/fileexchange/
@@ -124,21 +114,14 @@ class Calibraxis(object):
 
         """
         points = np.array(self._points)
-        if self._resolution is not None:
-            # The data sent in is assumed to be in the range
-            # [-2**(r - 1), 2**(r - 1) - 1], where r is the resolution in bits.
-            self._perform_accelerometer_calibration_optimisation(
-                self.acc_to_ratio(points))
-        else:
-            # The data sent in is already parsed to floats in the range
-            # [-x, +x], where x is the data range.
-            self._perform_accelerometer_calibration_optimisation(points)
+        self._perform_accelerometer_calibration_optimisation(points)
 
     def _perform_accelerometer_calibration_optimisation(self, points):
         """Perform the Gauss-Newton optimisation for parameters."""
         nbr_points = len(points)
         if nbr_points < 9:
-            raise ValueError('Need at least 9 measurements for the calibration procedure!')
+            raise ValueError(
+                'Need at least 9 measurements for the calibration procedure!')
 
         def error_function(M_mat, b_vec, y):
             """Optimisation error function for a point.
@@ -166,52 +149,70 @@ class Calibraxis(object):
             :rtype: float
 
             """
-            jac = np.zeros((9, ), 'float')
+            jac = np.zeros((9,), 'float')
 
             jac[0] = 2 * (b_vec[0] - point[0]) * (
-                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (b_vec[1] - point[1]) + M_mat[0, 2] * (
+                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (
+                b_vec[1] - point[1]) + M_mat[0, 2] * (
                     b_vec[2] - point[2]))
             jac[1] = 2 * (b_vec[1] - point[1]) * (
-                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (b_vec[1] - point[1]) + M_mat[0, 2] * (
+                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (
+                b_vec[1] - point[1]) + M_mat[0, 2] * (
                     b_vec[2] - point[2])) + 2 * (b_vec[0] - point[0]) * (
-                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (b_vec[1] - point[1]) + M_mat[1, 2] * (
+                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (
+                b_vec[1] - point[1]) + M_mat[1, 2] * (
                     b_vec[2] - point[2]))
             jac[2] = 2 * (b_vec[0] - point[0]) * (
-                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (b_vec[1] - point[1]) + M_mat[2, 2] * (
+                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (
+                b_vec[1] - point[1]) + M_mat[2, 2] * (
                     b_vec[2] - point[2])) + 2 * (b_vec[2] - point[2]) * (
-                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (b_vec[1] - point[1]) + M_mat[0, 2] * (
+                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (
+                b_vec[1] - point[1]) + M_mat[0, 2] * (
                     b_vec[2] - point[2]))
             jac[3] = 2 * (b_vec[1] - point[1]) * (
-                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (b_vec[1] - point[1]) + M_mat[1, 2] * (
+                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (
+                b_vec[1] - point[1]) + M_mat[1, 2] * (
                     b_vec[2] - point[2]))
             jac[4] = 2 * (b_vec[1] - point[1]) * (
-                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (b_vec[1] - point[1]) + M_mat[2, 2] * (
+                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (
+                b_vec[1] - point[1]) + M_mat[2, 2] * (
                     b_vec[2] - point[2])) + 2 * (b_vec[2] - point[2]) * (
-                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (b_vec[1] - point[1]) + M_mat[1, 2] * (
+                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (
+                b_vec[1] - point[1]) + M_mat[1, 2] * (
                     b_vec[2] - point[2]))
             jac[5] = 2 * (b_vec[2] - point[2]) * (
-                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (b_vec[1] - point[1]) + M_mat[2, 2] * (
+                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (
+                b_vec[1] - point[1]) + M_mat[2, 2] * (
                     b_vec[2] - point[2]))
             jac[6] = 2 * M_mat[0, 0] * (
-                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (b_vec[1] - point[1]) + M_mat[0, 2] * (
+                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (
+                b_vec[1] - point[1]) + M_mat[0, 2] * (
                     b_vec[2] - point[2])) + 2 * M_mat[0, 1] * (
-                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (b_vec[1] - point[1]) + M_mat[1, 2] * (
+                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (
+                b_vec[1] - point[1]) + M_mat[1, 2] * (
                     b_vec[2] - point[2])) + 2 * M_mat[0, 2] * (
-                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (b_vec[1] - point[1]) + M_mat[2, 2] * (
+                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (
+                b_vec[1] - point[1]) + M_mat[2, 2] * (
                     b_vec[2] - point[2]))
             jac[7] = 2 * M_mat[0, 1] * (
-                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (b_vec[1] - point[1]) + M_mat[0, 2] * (
+                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (
+                b_vec[1] - point[1]) + M_mat[0, 2] * (
                     b_vec[2] - point[2])) + 2 * M_mat[1, 1] * (
-                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (b_vec[1] - point[1]) + M_mat[1, 2] * (
+                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (
+                b_vec[1] - point[1]) + M_mat[1, 2] * (
                     b_vec[2] - point[2])) + 2 * M_mat[1, 2] * (
-                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (b_vec[1] - point[1]) + M_mat[2, 2] * (
+                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (
+                b_vec[1] - point[1]) + M_mat[2, 2] * (
                     b_vec[2] - point[2]))
             jac[8] = 2 * M_mat[0, 2] * (
-                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (b_vec[1] - point[1]) + M_mat[0, 2] * (
+                M_mat[0, 0] * (b_vec[0] - point[0]) + M_mat[0, 1] * (
+                b_vec[1] - point[1]) + M_mat[0, 2] * (
                     b_vec[2] - point[2])) + 2 * M_mat[1, 2] * (
-                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (b_vec[1] - point[1]) + M_mat[1, 2] * (
+                M_mat[0, 1] * (b_vec[0] - point[0]) + M_mat[1, 1] * (
+                b_vec[1] - point[1]) + M_mat[1, 2] * (
                     b_vec[2] - point[2])) + 2 * M_mat[2, 2] * (
-                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (b_vec[1] - point[1]) + M_mat[2, 2] * (
+                M_mat[0, 2] * (b_vec[0] - point[0]) + M_mat[1, 2] * (
+                b_vec[1] - point[1]) + M_mat[2, 2] * (
                     b_vec[2] - point[2]))
 
             return jac
@@ -234,17 +235,33 @@ class Calibraxis(object):
         nbr_iterations = 200
 
         # Initial Guess values of M and b.
-        x = np.array([self._acc_sensitivity[0], 0.0, 0.0,
-                      self._acc_sensitivity[1], 0.0, self._acc_sensitivity[2],
-                      self._acc_zero_g[0], self._acc_zero_g[1], self._acc_zero_g[2]])
+        if self.acc_bias_vector is not None:
+            # Recalibration using prior optimization results.
+            x = np.array([self.acc_scale_factor_matrix[0, 0],
+                          self.acc_scale_factor_matrix[0, 1],
+                          self.acc_scale_factor_matrix[0, 2],
+                          self.acc_scale_factor_matrix[1, 1],
+                          self.acc_scale_factor_matrix[1, 2],
+                          self.acc_scale_factor_matrix[2, 2],
+                          self.acc_bias_vector[0],
+                          self.acc_bias_vector[1],
+                          self.acc_bias_vector[2]])
+        else:
+            # Fresh calibration.
+            sensitivity = 1 / np.sqrt((points ** 2).sum(axis=1)).mean()
+            x = np.array([sensitivity, 0.0, 0.0,
+                          sensitivity, 0.0, sensitivity,
+                          0.0, 0.0, 0.0])
         last_x = x.copy()
+
         # Residuals vector
         R = np.zeros((nbr_points, ), 'float')
+
         # Jacobian matrix
         J = np.zeros((nbr_points, 9), 'float')
 
         for n in six.moves.range(nbr_iterations):
-            # Calculate the Jacobian at every iteration.
+            # Calculate the Jacobian and error for each point.
             M, b = optvec_to_M_and_b(x)
             for i in six.moves.range(nbr_points):
                 R[i] = error_function(M, b, points[i, :])
@@ -256,19 +273,49 @@ class Calibraxis(object):
             x -= gain * (D.dot(H)).T
             R_post = np.linalg.norm(R)
             if self._verbose:
-                print("{0}: {1} ({2})".format(n, R_post, ", ".join(["{0:0.9g}".format(v) for v in x])))
+                print("{0}: {1} ({2})".format(
+                    n, R_post, ", ".join(["{0:0.9g}".format(v) for v in x])))
 
-            # This is to make sure that the error is decreasing with every iteration.
+            # This is to make sure that the error is
+            # decreasing with every iteration.
             if R_post <= R_prior:
                 gain -= damping * gain
             else:
                 gain *= damping
 
-            # Iterations are stopped when the following convergence criteria is satisfied.
+            # Iterations are stopped when the following
+            # convergence criteria is satisfied.
             if abs(max(2 * (x - last_x) / (x + last_x))) <= tolerance:
-                self.acc_scale_factor_matrix, self.acc_bias_vector = optvec_to_M_and_b(x)
+                self.acc_scale_factor_matrix, self.acc_bias_vector = \
+                    optvec_to_M_and_b(x)
                 break
 
             last_x = x.copy()
             R_prior = R_post
             self._acc_calibration_errors.append(R_post)
+
+    def apply(self, acc_values):
+        """Apply the calibration scale matrix and bias to accelerometer values.
+
+        :param list, tuple, numpy.ndaray acc_values: The accelerometer data.
+        :return: The transformed accelerometer values.
+        :rtype: tuple
+
+        """
+        converted_g_values = self.acc_scale_factor_matrix.dot(
+            np.array(acc_values) - self.acc_bias_vector)
+        return tuple(converted_g_values.tolist())
+
+    def batch_apply(self, acc_values):
+        """Apply the calibration scale matrix and bias to an array of
+        accelerometer data
+
+        Assumes that the input is either a list or tuple containing three
+        element lists, tuples or arrays or a [N x 3] NumPy array.
+
+        :param list, tuple, numpy.ndaray acc_values: The accelerometer data.
+        :return: The transformed accelerometer values.
+        :rtype: list
+
+        """
+        return [self.apply(a) for a in acc_values]
